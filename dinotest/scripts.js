fbe773e16cd54a05cfecf665490bd813e1c05000
dinosaur20041108 <2843565830@qq.com>
2024-11-06 18:39:50 +0800

    add source

--- dinotest/scripts.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('nav a').forEach(item => {
-    item.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('导航功能尚未实现');
-    });
-});
